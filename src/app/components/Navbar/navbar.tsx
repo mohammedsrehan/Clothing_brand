4f9484001eb352afa455fafa4fadcc4e51d71c6e
mohammedsrehan <mohammedrehan275@gmail.com>
2025-07-20 19:55:00 -0400

    changing navbar logo

--- src/app/components/Navbar/navbar.tsx
+++ src/app/components/Navbar/navbar.tsx
@@ -5,8 +5,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
-        <Image src="/globe.svg" alt="Brand Logo" width={50} height={50} />
-        <h1>Clothing Brand</h1>
+        <Image src="/brand_logo.png" alt="Brand Logo" width={250} height={120} />
       </div>
       <div className={styles.navbar_links}>
         <a href="#cart">Featured</a>
