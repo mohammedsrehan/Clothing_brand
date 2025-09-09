ed7dd8b406059192809b06f707fcb6c606a7f374
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-08 22:22:47 -0400

    adding cart in the navbar

--- src/app/components/Navbar/navbar.tsx
+++ src/app/components/Navbar/navbar.tsx
@@ -1,17 +1,34 @@
 import Image from "next/image";
 import styles from "./navbar.module.css";
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
-        <Image src="/brand_logo.png" alt="Brand Logo" width={250} height={120} />
+        <Image
+          src="/brand_logo.png"
+          alt="Brand Logo"
+          width={250}
+          height={120}
+        />
       </div>
       <div className={styles.navbar_links}>
         <a href="#cart">Featured</a>
         <a href="#shop">Shop</a>
         <a href="/about">About</a>
         <a href="#contact">Contact</a>
+      </div>
+      <div>
+        <IconButton>
+          <ShoppingCartIcon fontSize="medium" />
+          <Badge sx={{
+            top: -12,
+            right: -6,
+          }} badgeContent={2} color="primary" overlap="circular" />
+        </IconButton>
       </div>
     </nav>
   );
