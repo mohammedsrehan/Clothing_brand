b07b4e54319bff3529806163cd0ee193ebf3a99b
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-08 22:22:02 -0400

    adding action button to featured product

--- src/app/components/Product/product.tsx
+++ src/app/components/Product/product.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./product.module.css";
+import { Button } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 const Product = () => {
   return (
     <div className={styles.product}>
@@ -18,6 +21,14 @@ const Product = () => {
         <p>Name of the Collection</p>
         <h3>Classic Shirt</h3>
         <p>$39.99</p>
+      </div>
+      <div className={styles.product_actions}>
+        <Button size="small" variant="outlined" startIcon={<AddShoppingCartIcon />} sx={{color: "black"}}>
+          Add to Cart
+        </Button>
+        <Button size="small" variant="contained" startIcon={<AddShoppingCartIcon />} sx={{color: "#fff", backgroundColor: "#4A9782"}}>
+          Buy Now
+        </Button>
       </div>
     </div>
   );
