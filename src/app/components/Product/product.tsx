3fd1816f76f26459206e72b970b746321636ddf5
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-20 22:18:44 -0400

    wrapping product component in link to route to product details page

--- src/app/components/Product/product.tsx
+++ src/app/components/Product/product.tsx
@@ -2,47 +2,58 @@ import Image from "next/image";
 import React from "react";
 import styles from "./product.module.css";
 import { Button } from "@mui/material";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Link from "next/link";
 
 interface ProductProps {
   product: {
     id: string;
-    collectionName: string; 
+    collectionName: string;
     productName: string;
     price: number;
     images: string[];
-  }
+  };
 }
 
-const Product = ({
-  product
-}: ProductProps) => {
+const Product = ({ product }: ProductProps) => {
   return (
-    <div className={styles.product}>
-      <div className={styles.image_container}>
-        <Image
-          fill
-          src={product.images[0] || "/landing_image.svg"}
-          alt="Classic Shirt"
-          objectFit="cover"
-          // width={180}
-          // height={220}
-        />
+    <Link href={`/products/${product.id}`} passHref>
+      <div className={styles.product}>
+        <div className={styles.image_container}>
+          <Image
+            fill
+            src={product.images[0] || "/landing_image.svg"}
+            alt="Classic Shirt"
+            objectFit="cover"
+            // width={180}
+            // height={220}
+          />
+        </div>
+        <div className={styles.product_details}>
+          <p>{product.collectionName}</p>
+          <h3>{product.productName}</h3>
+          <p>{product.price} Rs</p>
+        </div>
+        <div className={styles.product_actions}>
+          <Button
+            size="small"
+            variant="outlined"
+            startIcon={<AddShoppingCartIcon />}
+            sx={{ color: "black" }}
+          >
+            Add to Cart
+          </Button>
+          <Button
+            size="small"
+            variant="contained"
+            startIcon={<AddShoppingCartIcon />}
+            sx={{ color: "#fff", backgroundColor: "#4A9782" }}
+          >
+            Buy Now
+          </Button>
+        </div>
       </div>
-      <div className={styles.product_details}>
-        <p>{product.collectionName}</p>
-        <h3>{product.productName}</h3>
-        <p>{product.price} Rs</p>
-      </div>
-      <div className={styles.product_actions}>
-        <Button size="small" variant="outlined" startIcon={<AddShoppingCartIcon />} sx={{color: "black"}}>
-          Add to Cart
-        </Button>
-        <Button size="small" variant="contained" startIcon={<AddShoppingCartIcon />} sx={{color: "#fff", backgroundColor: "#4A9782"}}>
-          Buy Now
-        </Button>
-      </div>
-    </div>
+    </Link>
   );
 };
 
