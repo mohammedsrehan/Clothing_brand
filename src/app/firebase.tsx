26b9ce203acd2e5c660344cc7059346386363ffe
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-20 21:50:24 -0400

    adding firebase

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -1,13 +1,22 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
-  apiKey: process.env.Firebase_apiKey,
-  authDomain: process.env.Firebase_authDomain,
-  projectId: process.env.Firebase_projectId,
-  storageBucket: process.env.Firebase_storageBucket,
-  messagingSenderId: process.env.Firebase_messagingSenderId,
-  appId: process.env.Firebase_appId,
-  measurementId: process.env.Firebase_measurementId
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
+const auth = getAuth()
+const storage = getStorage(firebaseApp)
+
+export { firebaseApp, db, auth, storage }
