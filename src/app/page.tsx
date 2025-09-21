75c977f3a63e182f040e4b227289e170edd652a7
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-20 21:52:43 -0400

    displaying dynamic data in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar/navbar";
 import Product from "./components/Product/product";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
 export default function Home() {
+  const [data, setData] = useState<any[]>([]);
+  const getProducts = async () => {
+    // This function would typically fetch products from your database
+    const querySnapshot = await getDocs(collection(db, "products"));
+    const fetchedData: any[] = [];
+    querySnapshot.forEach((doc) => {
+      // setData =
+      fetchedData.push({ id: doc.id, ...doc.data() });
+    });
+    setData(fetchedData);
+    console.log(fetchedData);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -24,8 +45,8 @@ export default function Home() {
           <div className={styles.hero_content}>
             <h1 className={styles.title}>Elevate Your Style</h1>
             <p className={styles.tagline}>
-              Bismillah Collection Presents the latest trends in fashion with Quality, Comfort, and
-              Confidence.
+              Bismillah Collection Presents the latest trends in fashion with
+              Quality, Comfort, and Confidence.
             </p>
             <a href="#shop" className={styles.shop_button}>
               Shop Now
@@ -35,9 +56,13 @@ export default function Home() {
         <section id="shop" className={styles.featured}>
           <h2>Featured Products</h2>
           <div className={styles.featured_products}>
-            <Product />
-            <Product/>
-            <Product/>
+            {data.length === 0 ? (
+              <p>No products found</p>
+            ) : (
+              data.map((product) => (
+                <Product key={product.id} product={product} />
+              ))
+            )}
           </div>
         </section>
       </main>
