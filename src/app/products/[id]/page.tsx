e3985b3acf3ca25c431e89a49abc49f14f5d4b18
mohammedsrehan <mohammedrehan275@gmail.com>
2025-09-28 21:54:28 -0400

    resolving build conflicts

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,16 +1,16 @@
 // app/product/[id]/page.tsx
 import Navbar from '@/app/components/Navbar/navbar';
 import ProductDetails from '@/app/components/ProductDetails/productDetails';
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
 
-async function getProduct(id: string) {
-  // Replace with your actual API or data source
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-  if (!res.ok) return null;
-  return res.json();
-}
+// async function getProduct(id: string) {
+//   // Replace with your actual API or data source
+//   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+//   if (!res.ok) return null;
+//   return res.json();
+// }
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page() {
   // const product = await getProduct(params.id);
 
   // if (!product) return notFound();
